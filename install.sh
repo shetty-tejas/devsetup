0eddb3df37c61b9c092a54b8c28c7f128fe14251
Tejas <tejas.shetty@mailbox.org>
2025-05-15 10:50:44 +0530

    Code Quality, File Renames and Local Locations

--- install.sh
+++ install.sh
@@ -2,72 +2,81 @@
 
 set -e
 
-RAW_CONTENT_URL="https://raw.githubusercontent.com/shetty-tejas/devsetup/refs/heads/master"
-DESTINATION_FOLDER="/usr/local/bin"
 BINARY_NAME="devsetup"
+DESTINATION_FOLDER="$HOME/.local/bin"
+RAW_CONTENT_URL="https://raw.githubusercontent.com/shetty-tejas/devsetup/refs/heads/master"
+
 BINARY_FILE_LOCATION="$DESTINATION_FOLDER/$BINARY_NAME"
 
+# Utility Functions
+
+# Function to echo messages to STDERR.
+error() { printf "ERROR: %b\n" "$1" >&2; }
+# Function to echo messages to STDOUT.
+log() { printf "%b\n" "$1"; }
+
+# Command Functions.
+
+# Function to install devsetup.
 install() {
   if [ -e "$BINARY_FILE_LOCATION" ]; then
-    printf "Error: %s already exists in %s.\n\nOptions:\n  - To update: Run '%s update-self'.\n  - To add to PATH: Add '%s' to your PATH variable.\n\n" "$BINARY_NAME" "$DESTINATION_FOLDER" "$BINARY_NAME" "$DESTINATION_FOLDER"
-    return 1
+    error "'$BINARY_NAME' already exists in '$DESTINATION_FOLDER'.\n\nOptions:\n  - To update: Run '$BINARY_NAME update-self'.\n  - To add to PATH: Add '$DESTINATION_FOLDER' to your PATH variable."
+
+    exit 1
   fi
 
-  binary_url="$RAW_CONTENT_URL/$BINARY_NAME.sh"
+  binary_url="$RAW_CONTENT_URL/$BINARY_NAME"
   temp_file=$(mktemp)
 
-  echo "Installing devsetup..."
+  log "Downloading 'devsetup'..."
 
-  if type curl >/dev/null 2>&1; then
-    # Use -f to fail on server errors and -S to show errors
-    if ! curl -s -S -f -L "$binary_url" -o "$temp_file"; then
-      echo "Error: Failed to download the latest version"
+  if command -v curl >/dev/null 2>&1; then
+    if ! curl -s -S -f "$binary_url" -o "$temp_file"; then
+      error "Failed to download the latest version."
+
       rm -f "$temp_file"
-      return 1
+      exit 1
     fi
   else
-    echo "Error: curl not found."
+    error "'curl' not found."
+
     rm -f "$temp_file"
-    return 1
+    exit 1
   fi
 
   # Check if the download was successful and the file is not empty
   if [ ! -s "$temp_file" ]; then
-    echo "Error: Downloaded file is empty"
+    error "Downloaded file is empty."
+
     rm -f "$temp_file"
-    return 1
+    exit 1
   fi
-  
+
   # Make sure the downloaded file is a valid shell script
   first_line=$(head -n 1 "$temp_file")
 
-  if printf "%s" "$first_line" | grep -Eq '^#!.*[ /](bash|sh)'; then
+  if log "$first_line" | grep -Eq '^#!.*[ /](bash|sh)'; then
     :
   else
-    echo "Error: Downloaded file doesn't appear to be a valid shell script"
+    error "Downloaded file doesn't appear to be a valid shell script."
+
     rm -f "$temp_file"
-    return 1
+    exit 1
   fi
 
-  echo "Making directory at $DESTINATION_FOLDER if it doesn't exist."
-  
-  if ! mkdir -p "$DESTINATION_FOLDER"; then
-    rm -f "$temp_file"
-    echo "Error: Permission denied. Please run the script with 'sudo' command."
-    return 1
-  fi
+  log "Making directory at '$DESTINATION_FOLDER' if it doesn't exist..."
+  mkdir -p "$DESTINATION_FOLDER"
 
-  if ! touch "$BINARY_FILE_LOCATION"; then
-    rm -f "$temp_file"
-    echo "Error: Permission denied. Please run the script with 'sudo' command."
-    return 1
-  fi
-
-  cat "$temp_file" > "$BINARY_FILE_LOCATION"
+  log "Writing the script at '$BINARY_FILE_LOCATION'..."
+  touch "$BINARY_FILE_LOCATION"
+  cat "$temp_file" >"$BINARY_FILE_LOCATION"
   chmod +x "$BINARY_FILE_LOCATION"
+
+  log "Cleaning up..."
   rm -f "$temp_file"
 
-  printf "Installed %s at %s.\nPlease add this location to your PATH if not already done.\n" "$BINARY_NAME" "$DESTINATION_FOLDER"
+  log "Installed '$BINARY_NAME' at '$DESTINATION_FOLDER'.\nPlease add this location to your PATH if not already done."
+  exit 0
 }
 
 install
